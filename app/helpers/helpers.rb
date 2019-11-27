b088a13ed028f9e9e612a2e797e0035919c2f1ea
Mike Cooper <mikescooper@icloud.com>
2019-11-27 22:16:52 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,20 @@
 class Helpers
   
+<<<<<<< HEAD
   def self.current_user(session)
    User.find_by(id: session[:user_id]) 
   end 
   
   def self.is_logged_in?(session)
     !!session[:user_id]
+=======
+  def current_user
+    
+  end 
+  
+  def is_logged_in?
+    
+>>>>>>> 80bc0f4b08338dbeecd2e765aae0b33b9a9d181b
   end 
   
 end
